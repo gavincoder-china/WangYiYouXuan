ae58cfde5e5315ca7afebd8a257ffe69f76d6344
gavincoder <907516241@qq.com>
2019-10-18 14:24:28 +0800

    zmx

--- project/provider/src/main/java/com/wyyx/provider/dto/ProductOrder.java
+++ project/provider/src/main/java/com/wyyx/provider/dto/ProductOrder.java
@@ -7,49 +7,53 @@ import lombok.Data;
 @Data
 public class ProductOrder implements Serializable {
     /**
-    * 商品订单id
-    */
+     * 商品订单id
+     */
     private Long id;
 
     /**
-    * 用户id
-    */
+     * 用户id
+     */
     private Long userId;
 
     /**
-    * 商品id
-    */
+     * 商品id
+     */
     private Long productId;
 
-    /**
-    * 收货人姓名
-    */
-    private String receiverName;
+    private String name;
 
     /**
-    * 收货人手机号
-    */
+     * 收货人手机号
+     */
     private String receiverPhone;
 
     /**
-    * 收获人地址
-    */
+     * 收获人地址
+     */
     private String receiverAddress;
 
     /**
-    * 订单状态,0取消,1待支付,2支付成功,3支付失败,4已收货,5退货中,6已退货
-    */
+     * 订单状态,0取消,1待支付,2支付成功,3支付失败,4已收货,5退货中,6已退货
+     */
     private Byte state;
 
+    private Boolean isDelete;
+
     /**
-    * 支付方式(如微信支付)
-    */
+     * 该条记录的创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 支付方式(如微信支付)
+     */
     private String payType;
 
     /**
-    * 该条记录的创建时间
-    */
-    private Date createTime;
+     * 收货人姓名
+     */
+    private String receiverName;
 
     private static final long serialVersionUID = 1L;
 }
