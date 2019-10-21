1277c8fcb675ea09aa6751e454c542f8396d0483
gavincoder <907516241@qq.com>
2019-10-21 13:40:53 +0800

    zmx

--- project/consumer/src/main/java/com/wyyx/consumer/vo/UserVo.java
+++ project/consumer/src/main/java/com/wyyx/consumer/vo/UserVo.java
@@ -1,5 +1,6 @@
 package com.wyyx.consumer.vo;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -20,12 +21,23 @@ public class UserVo implements Serializable {
 
     private static final long serialVersionUID = -567136965701406919L;
 
+    @ApiModelProperty(value = "缓存用户id")
     private Long userID;
+
+    @ApiModelProperty(value = "缓存用户名")
     private String userName;
+
+    @ApiModelProperty(value = "缓存用户手机号")
     private String phone;
+
+    @ApiModelProperty(value = "缓存用户密码")
     private String password;
+
     //是否同意,0标识不同意,其他数字表示同意
+    @ApiModelProperty(value = "是否同意(0-不同意，1-同意)")
     private int isAgree = 1;
+
+    @ApiModelProperty(value = "临时token")
     private String temp;
 
 
